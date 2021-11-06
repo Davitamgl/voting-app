c9ef28dcaf6e4b553533d943eb13db9f08befbe2
Davit Amaghlobeli <amaghlobeli.davit@gmail.com>
2021-11-06 18:10:00 +0400

    feat( idea/Model ): implement sluggable

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -4,14 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Idea extends Model
 {
-    use HasFactory;
-    protected $guarded =  [];
+	use HasFactory, Sluggable;
 
-    public function user()
+	protected $guarded = [];
+
+	/**
+	 * Return the sluggable configuration array for this model.
+	 *
+	 * @return array
+	 */
+	public function sluggable(): array
 	{
-        return $this->belongsTo(User::class);
+		return [
+			'slug' => [
+				'source' => 'title',
+			],
+		];
+	}
+
+	public function user()
+	{
+		return $this->belongsTo(User::class);
 	}
 }

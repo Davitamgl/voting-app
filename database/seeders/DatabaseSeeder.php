601f212991f80cc4a395c785e1f9d18656fed46a
Davit Amaghlobeli <amaghlobeli.davit@gmail.com>
2021-11-07 16:14:10 +0400

    feat: add relationship between idea and category
    add relationship between idea and category, update migrations, update database seeder with categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,20 +2,24 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Idea;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-        Idea::factory(30)->create();
-    }
+	/**
+	 * Seed the application's database.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Category::factory()->create(['name' => 'Category 1']);
+		Category::factory()->create(['name' => 'Category 2']);
+		Category::factory()->create(['name' => 'Category 3']);
+		Category::factory()->create(['name' => 'Category 4']);
+
+		Idea::factory(30)->create();
+	}
 }
- 

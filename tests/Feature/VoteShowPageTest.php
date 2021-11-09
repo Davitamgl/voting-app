c0917b9115f72dde1454ffa7f41e9466f7215960
Davit Amaghlobeli <amaghlobeli.davit@gmail.com>
2021-11-09 20:46:17 +0400

    test: update tests with vote

--- tests/Feature/VoteShowPageTest.php
+++ tests/Feature/VoteShowPageTest.php
@@ -62,7 +62,35 @@ class VoteShowPageTest extends TestCase
 	}
 
 	/** @test */
-	public function votes_count_shows_correctly_on_show_page_livewire_component()
+	public function user_who_is_logged_in_shows_voted_if_idea_already_voted_for()
+	{
+		$user = User::factory()->create();
+		$categoryOne = Category::factory()->create(['name' => 'Category 1']);
+		$statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+		$idea = Idea::factory()->create([
+			'user_id'     => $user->id,
+			'category_id' => $categoryOne->id,
+			'status_id'   => $statusOpen->id,
+			'title'       => 'My First Idea',
+			'description' => 'Description for my first idea',
+		]);
+
+		Vote::factory()->create([
+			'idea_id' => $idea->id,
+			'user_id' => $user->id,
+		]);
+
+		Livewire::actingAs($user)
+		->test(IdeaShow::class, [
+			'idea'       => $idea,
+			'votesCount' => 5,
+		])
+		->assertSet('hasVoted', true)
+		->assertSee('Voted');
+	}
+
+	/** @test */
+	public function user_who_is_not_logged_in_is_redirected_to_login_page_when_trying_to_vote()
 	{
 		$user = User::factory()->create();
 		$categoryOne = Category::factory()->create(['name' => 'Category 1']);
@@ -79,8 +107,7 @@ class VoteShowPageTest extends TestCase
 			'idea'       => $idea,
 			'votesCount' => 5,
 		])
-			->assertSet('votesCount', 5)
-			->assertSeeHtml('<div class="text-xl leading-snug">5</div>')
-			->assertSeeHtml('<div class="text-sm font-bold leading-none">5</div>');
+			->call('vote')
+			->assertRedirect(route('login'));
 	}
 }

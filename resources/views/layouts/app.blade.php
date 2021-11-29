b754136def3c4af1835ef4db8618b0a2cef9c802
Davit Amaghlobeli <amaghlobeli.davit@gmail.com>
2021-11-29 18:26:38 +0400

    refactor: use jit and blend mode luminosity

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,11 +52,13 @@
                     @endauth
                 </div>
             @endif
+            
+            @auth
             <a href="#">
-                <img src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp" alt="avatar"
-                    class="w-10 h-10 rounded-full">
+                <img src="{{ auth()->user()->getAvatar() }}" alt="avatar" class="w-10 h-10 rounded-full">
             </a>
-
+          @endauth
+            
         </div>
     </header>
     <main class="container flex flex-col mx-auto max-w-custom md:flex-row">
--- resources/views/livewire/idea-comments.blade.php
+++ resources/views/livewire/idea-comments.blade.php
@@ -11,8 +11,7 @@
         </div>
     @else
         <div class="mx-auto mt-12 w-70">
-            <img src="{{ asset('img/no-ideas.svg') }}" alt="No Ideas" class="mx-auto"
-                style="mix-blend-mode: luminosity">
+            <img src="{{ asset('img/no-ideas.svg') }}" alt="No Ideas" class="mx-auto mix-blend-luminosity">
             <div class="mt-6 font-bold text-center text-gray-400">No comments yet...</div>
         </div>
     @endif
